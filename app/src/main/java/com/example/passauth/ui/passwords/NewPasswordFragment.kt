b32444aea8549e542add6a5e092d5ffe19c02164
petar98 <petaryanakiev.py@gmail.com>
2020-08-07 17:40:06 +0300

    added input field validation

--- app/src/main/java/com/example/passauth/ui/passwords/NewPasswordFragment.kt
+++ app/src/main/java/com/example/passauth/ui/passwords/NewPasswordFragment.kt
@@ -1,6 +1,7 @@
 package com.example.passauth.ui.passwords
 
 import android.content.Context
+import android.graphics.Color
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,6 +9,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat.getColor
 import androidx.core.content.ContextCompat.getSystemService
 import com.example.passauth.R
 import com.example.passauth.databinding.FragmentNewPasswordBinding
@@ -29,6 +33,19 @@ class NewPasswordFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentNewPasswordBinding.inflate(inflater, container, false)
+
+        binding.saveButton.setOnClickListener {
+            val appName = binding.inputApplication.text.toString()
+            if (appName == "") {
+                Toast.makeText(context, "Application name is required", Toast.LENGTH_SHORT).show()
+                binding.inputApplication.setHintTextColor(Color.RED)
+                binding.inputApplication.setOnFocusChangeListener { v, hasFocus ->
+                    if (hasFocus) {
+                        binding.inputApplication.setHintTextColor(getColor(context!!, R.color.colorAccent))
+                    }
+                }
+            }
+        }
         return binding.root
     }
 
